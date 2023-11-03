87ce385338f32b9135295cc1c21fd03cfd1bbec1
saczky <krystian.sak@infotech.edu.pl>
2023-11-03 14:29:07 +0100

    formatoawnie i formularze

--- globomantics/website/views.py
+++ globomantics/website/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from datetime import datetime
 from website.models import Meeting, Room
+from django.forms import modelform_factory
+
+Meetingform = modelform_factory(Meeting, exclude=[]) 
 
 def detail(request, id):
     meeting=Meeting.objects.get(pk=id)
@@ -19,3 +22,9 @@ def omnie(request):
 def pokoje(request):
     rooms = Room.objects.all()
     return render(request, "website/pokoje.html", {'rooms': rooms})
+
+def new(request):
+    form = Meetingform()
+    return render(request, "website/new.html", {"form":form})
+
+
